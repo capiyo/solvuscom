ce53a045bbbd10033f07af3b01dfc329c95f6bfc
Capiyo <apiyoosca@gmail.com>
2025-07-29 18:33:11 +0300

    update

--- src/components/Home/Pending.jsx
+++ src/components/Home/Pending.jsx
@@ -2,267 +2,378 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logoURL from '../../assets/img/laptop.jpeg'
-import { toast } from 'react-toastify';
+import {FaBeer,FaBrain,FaTree,FaHeart, FaEye, FaRedRiver, FaMale, FaHandHolding}  from "react-icons/fa"
+import { ToastContainer,toast } from 'react-toastify'
 import { LoginContext } from '../ContextProvider/Context';
-import { useDispatch, useSelector } from 'react-redux';
-export const Pending= () => {
+import { useDispatch,useSelector} from "react-redux"
 
-    const [applicants, setApplicants] = useState([]);
-    const[loginData, setLoginData]=useState(LoginContext)
 
-  
-    const[myId,setMyId]=useState("")
-      useEffect(() => {
+export const Pending = () => {
 
-        let token = localStorage.getItem("user");
-        const user = JSON.parse(token);
+    const [jobs, setJobs] = useState([]);
 
-        setMyId(user.userId)
-        setLoginData(user)
+   
+   const [apaiChecker,setApiChecker]=useState(false)
+   const[likes,setLikes]=useState([])
 
-        console.log(loginData+"hereeeee")
-    }, [])
-  
+
+   
+    
 
     useEffect(() => {
-        fetch("https://solvus-api-4.onrender.com/jobs/all-jobs").then(res => res.json()).then(
-            data => {
-                const newData = data
-                setApplicants(newData)
-                console.log(newData)
 
+         fetch("https://solvus-api-4.onrender.com/jobs/all-jobs").then(res => res.json()).then(
+            data => {
+                //const newData = data
+                setJobs(data)
+              console.log(data)
+                
+               
             }
-        ).catch((error)=>console.log(error));
-        
+        );
     }, []);
 
+
+    /*useEffect(() => {
+        console.log(jobs.jobID)
+
+         fetch(`http://localhost:5000/jobs/getlikes/${jobs.jobID}`).then(res => res.json()).then(
+            allLikes => {
+                const newData = allLikes
+                setLikes(newData)
+                console.log(likes)
+              
+            }
+        );
+    }, []);*/
+
+
+
+
+const loadLikes=(jobId)=>{
+    fetch(`http://localhost:5000/jobs/getlikes/${jobId}`).then(res => res.json()).then(
+            data => {
+                const newData = data
+                setLikes(newData)
+                console.log(likes)
+               // console.log(LoginContext._id)
+
+            }
+        );
+
+} 
+
+
+
+
+        
+       
+
     
 
     return (
-        <div className=' w-screen flex-col absolute bottom-10 h-[600px] overflow-y-auto bg-green-300'>
-            <h1 className='text-center text-xl     md:text-2xl font-bold text-primary mt-8 md:mt-6   '>Chat Histoy</h1>
+     <div className=' w-screen flex-col absolute bottom-10 h-[600px] overflow-y-auto bg-green-300'>
+            <h1 className='text-center text-xl  h-[20px]   md:text-2xl  text-primary mt-2 md:mt-6   '>Pending Gigs</h1>
             <div className='grid sm:grid-cols-1 md:grid-cols-1   container mt-2 mx-auto xl:px-24 px-4 bg-white  rounded-xl '>
-                {applicants.map((people, key) => <Card key={key}  applicants={people} />)}
+         {jobs.map((job, key) => <Card    key={key} job={job} />)}
             </div>
         </div>
+       
     )
 }
+//bg-[#e0F8FF]
 
-function Card({ applicants }) {
-      const[assign,setAssin]=useState(false)
-        const[workerId,setWorkerId]=useState('')
-        const[posterId,setPosterId]=useState("")
-        const[workerName,setWorkername]=useState("")
-        const[loginData,setLoginData]=useState('')
-    const[bossPhone,setBossPhone]=useState()
-           const caseId=useSelector((state)=>state.caseData['caseId'])
-           //const  caseBudget=useSelector((state) =>state.caseData['budget'])
-           const caseTitel=useSelector((state)=>state.caseData["caseTitle"])
-
-    const mybudget=useSelector((state)=>state.caseData["budget"])
-
-           //console.log(gigStatus)
-           
-
-        
-        //const[]
-    const  confirmAssign=()=>{
-        setAssin(true)
-    }
-
-    function getPosterDetails(){
-
-    }
-    
-
-    function getMessApplicantsData(userName,userId,userEmail){
-
-         const  messageData={
-        workerId:userId,
-        posterId:myId,
-        caseId:caseId,
-        caseTitle:caseTitel,
-        workerEmail:userEmail,
-        workerName:userName,
-      posterName:posterName,
-      posterEmail:posterEmail,
-        message:"Congratulations  you are assgined the gig  od Angular dev from @Capiyo",
-        status:"Started"
-    }
-       fetch("http://localhost:5000/case/addWorkerChats", {
-            method: "POST",
-            headers: {'content-type' : 'application/json'},
-            body: JSON.stringify(messageData)
-            
-        })
-        .then((res) => res.json())
-        .then((result) => {
-            console.log(result);
-            setAssin(false)
-            toast.success("Notiefiled successfully kindly  wait for his reply in your inbox or check thread chat")
-            //window.location.href = '/all-jobs';
-        })
-        .catch((error) => {
-            console.log(error);
-            toast.error("Failed to  To Notify,Please try again}");
-            setAssin(false)
-        });
-
-
-   // console.log(messageData)
-    
-        
-
-
-
-    }
-
-
-
-
-
+function Card({ job}) {
+     const[bgColor1,setbg1Color]=useState('bg-gradient-to-r from-purple-300 to-blue-500')
+     const[bgColor2,setbg2Color]=useState(' bg-gradient-to-r from-green-300 to-red-300')
+     const[bgColor3,setbg3Color]=useState('bg-gradient-to-r from-purple-100 to-black')
+     const[request,setRequest]=useState(false)
+     const [mainBg,setMainbg]=useState("bg-gradient-to-b from-white to-gray")
+     const[navebg,setNavBg]=useState()
+     const[loginData,setLoginData]=useState(LoginContext)
      const[myId,setMyId]=useState("")
-     const[posterName,setPostername]=useState("")
-     const [posterEmail,setPosterEMail]=useState("")
-      useEffect(() => {
+     const[myName,setMyname]=useState("")
+     const[workerEmail,setWorkerEmail]=useState("")
+         const[btnBg,setBg]=useState("bg-blue-700")
+       const   dispatch=useDispatch()
 
-        let token = localStorage.getItem("user");
+ 
+
+
+useEffect(() => {
+           let token = localStorage.getItem("user");
         const user = JSON.parse(token);
+        setLoginData(user)  
+        //console.log(user.userId)
         setMyId(user.userId)
-        setPostername(user.userName)
-        setPosterEMail(user.userEmail)
-          setBossPhone(user.phoneNumber)
-        setLoginData(user)
+        setMyname(user.userName)
+        setWorkerEmail(user.userEmail)
 
-        console.log(myId,posterEmail,posterName,bossPhone)
-    }, [myId])
+      
+       // console.log(LoginContext["userId"])
+       // console.log(typeof(LoginContext))
+        //setBossId(loginData.userId)
+       // console.log(bossId+"The capiyo")
+    }, [])
 
+    function changeMain(){
+        setMainbg("bg-gradient-to-b from-white to-gray-300")
+        setRequest(true)
+        setbg1Color("bg-gradient-to-r from-purple-300 to-geen-300")
+        setbg2Color("bg-gradient-to-r from-blue-300 to-red-300")
+        setbg3Color("bg-gradient-to-r from-blue-300 to-yelow-300")
 
+    
 
-
-
-const asssignNow =()=>{
-    setAssin(false)
-   // toast('Task Assign successfully')
-    //startChart()
-}
-
-const cancelAssign=()=>{
-    setAssin(false)
-}
-const sendPayments=(username,userId,userEmail)=>{
-    const amount=20
-    //const phonenumber=
-    const paymentData={
-        budget:mybudget,
-        phoneNumber:bossPhone
     }
-console.log(paymentData)
 
-    fetch("http://localhost:5000/payment/stk/push", {
+    function returnBg(){
+        setMainbg("bg-gradient-to-b from-white to-gray-700")
+         setRequest(false)
+              }
+
+
+
+
+
+     
+    function onClick(){
+    if(bgColor1){
+        setbg1Color("bg-gradient-to-r from-red-800 to-[#06402b]")
+    }
+    else{
+        setbg1Color('bg-gradient-to-r from-red-800 to-yellow-500')
+    }
+
+   }
+
+    function buttonEnter(myValue){
+        if(myValue===1){
+            console.log(myValue)
+        
+        
+    }
+    else if(myValue===2){
+        setbg2Color('bg-gradient-to-r from-green-100 to-blue-500')
+          console.log(myValue)
+    }
+    else if(myValue===3){
+        setbg3Color('bg-gradient-to-r from-red-300 to-green-200')
+          console.log(myValue)
+
+    }
+    }
+
+ function buttonOut(myValue){
+        if(myValue===1){
+            console.log(myValue)
+        setbg1Color("bg-gradient-to-r from-purple-300 to-blue-300")
+        
+    }
+    else if(myValue===2){
+        setbg2Color('bg-gradient-to-r from-green-300 to-blue-300')
+          console.log(myValue)
+    }
+    else if(myValue===3){
+        setbg3Color('bg-gradient-to-r from-purple-100 to-black')
+          console.log(myValue)
+
+    }
+}
+
+
+
+
+
+
+   
+    const  submitLikes=(event)=>{
+        const  jobId=event
+        console.log(myId)
+        console.log(job.posterId)
+
+       /* const output={
+            likes:10,
+            jobId
+        }
+       console.log(output)
+
+
+
+           fetch("http://localhost:5000/jobs/addlikes", {
             method: "POST",
             headers: {'content-type' : 'application/json'},
-            body: JSON.stringify(paymentData)
+            body: JSON.stringify(output)
             
         })
         .then((res) => res.json())
         .then((result) => {
             console.log(result);
-           // getMessApplicantsData(username,userId,userEmail)
-            ////getMessApplicantsData()
-           // setAssin(false)
-          //  toast.success("Notiefiled successfully kindly  wait for his reply in your inbox or check thread chat")
-            //window.location.href = '/all-jobs';
+            toast.success("Like Submitted Successfully")
+            window.location.href = '/all-jobs';
         })
         .catch((error) => {
             console.log(error);
-            toast.error("Payment Failed  kindly try again later");
-            setAssin(false)
+            toast.error("Failed to post job");
+        });   
+*/
+        
+
+    }
+const changebg=()=>{
+
+
+}
+
+const showRequest=(jobId)=>{
+    const output={
+    "jobId":jobId,
+    "workerName":myName,
+    "workerId":myId,
+    "workerEmail":workerEmail,
+    }
+    
+    
+                fetch("https://solvus-api-4.onrender.com/jobs/add-applicants", {
+            method: "POST",
+            headers: {'content-type' : 'application/json'},
+            body: JSON.stringify(output)
+            
+        })
+        .then((res) => res.json())
+        .then((result) => {
+            console.log(result);
+            toast("Your Request was sent successfully")
+
+            //setRequest(true)
+          //  window.location.href = '/all-jobs';
+        })
+        .catch((error) => {
+            console.log(error);
+            toast.error("Failed to aplly");
         });
 
-
-
-
-
-
+    console.log(jobId)
+    console.log(myId)
+    console.log(myName)
 }
 
 
 
 
+
+
+      const getGigData=(gigTitle,gigId,budget,status)=>{
+        let   caseTitle=gigTitle
+        const data={
+            caseTitle:gigTitle,
+            caseId:gigId,
+            budget:budget
+        }
+
+                 dispatch({type:"caseData",payload:data})
+                 dispatch({type:"gigStatus",payload:status})
+
+
+
+                 console.log(data)
+
+            }
 
     return (
-
-
-        <div className='border shadow-lg hover:border-green-300    w-full    lg:w-[700px]   rounded-xl flex-row  bg-green-100 card'>
+        <div      className={`border items-center shadow-sm     card     bg-green-300
+        rounded-xl `}>
           
-        
-            
-          <div className='flex items-center gap-3'>
+            {/* Card Header */}
+            <div className='flex items-center   gap-1'>
                 <div>
                     {/* company image */}
-                    <img src={logoURL} alt={applicants.workerName} className='rounded-full w-12' />
+                    <img src={logoURL} alt={job.companyName} className='w-8 rounded-full' />
                 </div>
                 <div>
-                    <div className='flex items-center'>
-                        <box-icon size='18px' name='time'></box-icon>
-                        <span className='pl-1 text-blue-800'>{applicants.workerName} </span>
+                    <div className='flex '>
+                           <svg class="w-4 h-10 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                              d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 
+                              2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>                      
+                    </svg>
+                          
+                       
+                   
+                      
                     </div>
-                    <h1 className='font-bold text-md lg:text-lg'>{applicants.workerEmail}</h1>
+                    <h1 className=' sm:text-[12px]   text-md text-blue-500'>{job.jobTitle}</h1>
                 </div>
             </div>
-            <div>
-                <p className='text-sm py-4'>{applicants.workerEmail}</p>
+            <div className='flex flex-col'>
+                <FaBrain/>
+                <p className='py-4  lg:text-lg    sm:text-sm/8'>{job.description}</p>
             </div>
-            {/* Footer - apply now and location */}
-            <div className='flex justify-between items-center'>
-                <div className='flex justify-center items-center'>
-                    <box-icon size='19px' name='pin'></box-icon>
-                    <span className='pl-2'>{applicants.workerEmail} </span>
-                </div>
-                
-                            
-            </div>
-               <div className='flex flex-row  justify-evenly w-100%'>
-                
-                <div>
-                    <button className=' lg:block bg-green-500 text-white text-sm py-1 px-4 rounded-md'>View Profile</button>
-                </div>
-                <div>
-                    <button  onClick={confirmAssign} className=' lg:block bg-green-500 text-white text-sm py-1 px-4 rounded-md'>Assign task</button>
-                </div>
-                </div>
+            {/* Footer - apply now and location */}            
+            <div className={`flex justify-between flex-row ` }> 
 
-                <div>
-                {assign?
-                        <div className='flex bg-white flex-col  p-2 rounded-xl  -rotate-20  sticky'>
-                            <div className='flex'>
-                           <div className='flex flex-row'> <p>Confirm Payment before assigning    <span className=' font-bold text-red-800  font-bold '>
-                            {`@${applicants.workerName}`}   </span> ,once assigned No reassigng  </p></div>
-                            <img src={logoURL} alt='Logog' w className='w-12 rounded-full'/>
-                            </div>
+                {job.posterId===myId?    <Link to={`/current-job/${job._id}`}>
+                    <div      onMouseLeave={(myValue)=>buttonOut(1)} onMouseEnter={()=>buttonEnter(1)}
+                                 onClick={(gigTitle,gigId)=>getGigData(job.jobTitle,job.jobID,job.budget,job.status)} className={`lg:block  text-blue-900 font-bold
+                     text-white text-sm    mt-10 `}>
+                        <div    className='flex flex-row  text-purple-400   sm:text-[9px]  font-bold'><FaEye/>view </div>
+                        </div>
+                        
+                </Link>
+                : <div>
+               
+                    <div  onMouseLeave={(myValue)=>buttonOut(2)}   onMouseEnter={(myValue)=>buttonEnter(2)}className={`lg:block  
+                     text-white text-sm py-1 px-4 mt-10   `}>
+
+                           <div    className='sm:text-[5px] flex flex-row  text-violet-900 '><FaRedRiver/>follow</div>
+                      
+                        </div>
+                </div>}
 
 
-                             <div className='flex flex-row  justify-evenly w-100%'>
                 
-                <div>
-                    <button  onClick={cancelAssign}           className=' lg:block bg-primary text-white text-sm py-1 px-4 rounded-md'>Cancel</button>
+            
+
+                 <div>
+               
+                    <div  onMouseLeave={(myValue)=>buttonOut(2)}   onMouseEnter={(myValue)=>buttonEnter(2)}className={`lg:block  
+                     text-white text-sm    mt-10`}>
+
+                           <div    className='flex flex-row  text-green-500  sm:text-[10px] '><FaMale/>@{job.posterName}</div>
+                      
+                        </div>
                 </div>
-                <div>
-                <button     onClick={(username,userId,userEmail)=>sendPayments(applicants.workerName,applicants.workerId,applicants.workerEmail)}
-                className=' lg:block bg-primary text-white text-sm py-1 px-4 rounded-md'>Confirm</button>
+
+                 <div >
+                    <div onClick={(event)=>showRequest(job._id)}  onMouseEnter={(myValue)=>buttonEnter(3)}  onMouseLeave={(myValue)=>buttonOut(3)} className={`lg:block 
+                     text-white text-sm py-1 px-4  mt-10   `}>
+                        <div    className='flex flex-row text-blue-500  sm:text-[9px] '><FaHandHolding/>Be Admin</div> </div>
+                        
+                </div> 
+                  
+                 
                 </div>
-                </div>
+               
+                 <span className='font-extrabold text-xl flex flrx-row  mt-30 md:text-3xl text-green-500 sm:text-[5px] '>
+                        <span  onClick={(event)=>submitLikes(job._id)} className='text-sm sm:text-[5px] '>#36</span> <FaHeart/>   
                            
+                         
+                        </span>
 
+{request?
+                        <div className='flex bg-zinc-300 w-[200px]p-1 rounded-xl absolute -rotate-20'>
+                           <div className='flex flex-col'> <p>Hello   <span className='text-blue-800  font-bold '>{`@${myName}    `}</span>Your request has been sent Successfully,wait for notification.Thanks</p></div>
+                            <img src={logoURL} alt='Logog' w className='w-12 rounded-full'/>
                             </div>       
-                            :""}        
-                            </div>       
+                            :""}               
 
-                                  
-                            
-        </div>
-    
+
+                    
+                      
+
+                     
+            
+          
+              </div>
     )
-    
 }
