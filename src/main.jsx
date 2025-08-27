3b7c0f3e110355fd578b85274d1b63b113408086
Capiyo <apiyoosca@gmail.com>
2025-08-27 10:45:40 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { reducers } from './components/ReduxPages/reducers';
 import  {thunk} from 'redux-thunk';
 import { Login } from './components/Login/Login';
 import { Sidebar } from 'lucide-react';
-import Index from './components/Home/index'
+
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 const root = ReactDOM.createRoot(document.getElementById('root'));
